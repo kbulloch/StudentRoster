3b4571e437c7b5b305ed2da01a0fa507e63af054
Brett Moore & Kyle Bulloch <student@epicodus.com>
2015-04-20 10:02:08 -0700

    add initial files, add angular to display our student Roster

---
+++ js/StudentsController.js
@@ -0,0 +1,14 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+    $scope.students = [
+        { name: "Smicky McCoodle" },
+        { name: "Ted Nugent "},
+        { name: "Francis Bacon" },
+        { name: "Hans Olo" },
+        { name: "Sharon Peters" },
+        { name: "Sharon Needles" },
+        { name: "Larry Kryzinsky" },
+        { name: "Greg Alohia "},
+        { name: "Tim Tilliwink"},
+        { name: "Woodsley Hardington"}
+    ]
+});
