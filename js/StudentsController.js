4670b386ca8f02c9b7575a6fb78ebdfcd8c827eb
Brett Moore & Kyle Bulloch <student@epicodus.com>
2015-04-20 10:36:29 -0700

    complete two way binding and filtering through the students list

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -10,5 +10,13 @@ studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
         { name: "Greg Alohia "},
         { name: "Tim Tilliwink"},
         { name: "Woodsley Hardington"}
-    ]
+    ];
+    $scope.addStudent = function() {
+        $scope.students.push({ name: $scope.studentName });
+        $scope.studentName = null;
+    };
+    $scope.deleteStudent = function(student) {
+        var index = $scope.students.indexOf(student);
+        $scope.students.splice(index, 1);
+    };
 });
